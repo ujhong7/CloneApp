b149fdcf2b8d00ef3a72ba5c85e7d575b00d2189
ujhong7 <>
2023-04-13 21:15:14 +0900

    Instagram

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by yujaehong on 2023/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    // test
+    
+    
+    
+}
+
